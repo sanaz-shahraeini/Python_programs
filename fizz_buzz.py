628ac9d4f1d0bb6c5f4bd75a43c3cb4c36b06f04
s.shahraeini@yahoo.com <s.shahraeini@yahoo.com>
2022-12-05 14:10:14 +0330

    add comments

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,6 +1,23 @@
+#### fizzbuzz Game
+
+fizz_buzz = []
+i = 1
+while i <= 100:
+    if i % 15 == 0:
+        fizz_buzz.append("fizzbuzz")
+    elif i % 3 == 0:
+        fizz_buzz.append("fizz")
+    elif i % 5 == 0:
+        fizz_buzz.append("Buzz")
+    else:
+        fizz_buzz.append(i)
+    i += 1
+print(fizz_buzz)
+# for i in fizz_buzz:
+#     print(i)
+#----------------
 # fizz_buzz = []
-# i = 1
-# while i <= 100:
+# for i in range(1, 101):
 #     if i % 15 == 0:
 #         fizz_buzz.append("fizzbuzz")
 #     elif i % 3 == 0:
@@ -13,18 +30,3 @@
 # print(fizz_buzz)
 # for i in fizz_buzz:
 #     print(i)
-#----------------
-fizz_buzz = []
-for i in range(1, 101):
-    if i % 15 == 0:
-        fizz_buzz.append("fizzbuzz")
-    elif i % 3 == 0:
-        fizz_buzz.append("fizz")
-    elif i % 5 == 0:
-        fizz_buzz.append("Buzz")
-    else:
-        fizz_buzz.append(i)
-    i += 1
-print(fizz_buzz)
-for i in fizz_buzz:
-    print(i)
